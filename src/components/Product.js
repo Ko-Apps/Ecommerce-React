975a224710aa9ebfd064af0ac7afb7cb2bbdca40
Voltis <v.agolli@live.com>
2019-01-31 11:43:34 +0100

    product comp logic

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,10 +9,31 @@ class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={() => console.log('clicked')}>
+                    <div className="img-container p-5" onClick={() => console.log('clicked image container')}>
                         <Link to="/details" >
                             <img src={img} alt="product" className="card-img-top"></img>
                         </Link>
+                        <button
+                            className="cart-btn"
+                            disabled={inCart ? true : false}
+                            onClick={() => { console.log('added to cart'); }}
+                        >
+                            {
+                                inCart
+                                    ? (<p className="text-capitalize mb-0" disabled>{" "} in cart</p>)
+                                    : (<i className="fas fa-cart-plus" />)
+                            }
+                        </button>
+                    </div>
+                    {/* card footer */}
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">$</span>
+                            {price}
+                        </h5>
                     </div>
                 </div>
             </ProductWrapper>
