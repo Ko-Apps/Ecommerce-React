4a419dcddb8a0a576c01d434ba33c4af63c100f6
Voltis <v.agolli@live.com>
2019-01-31 14:46:53 +0100

    add to cart

--- src/context.js
+++ src/context.js
@@ -8,7 +8,8 @@ class ProductProvider extends Component {
 
     state = {
         products: [],
-        detailProduct
+        detailProduct,
+        cart: []
     }
 
     componentDidMount() {
@@ -35,12 +36,23 @@ class ProductProvider extends Component {
     handleDetail = (id) => {
         const product = this.getItem(id);
         this.setState(() => {
-            return { detailProduct: product }
+            return { detailProduct: product };
         })
     }
 
     addToCart = (id) => {
-        console.log('hello from cart with id: ' + id);
+        let tempProducts = [...this.state.products];
+        const index = tempProducts.indexOf(this.getItem(id));
+        const product = tempProducts[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
+        this.setState(() => {
+            return { products: tempProducts, cart: [...this.state.cart, product] };
+        }, () => {
+            console.log(this.state);
+        });
     }
 
     render() {
