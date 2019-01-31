8773fd60ac99def20f931de36939a09a0c594075
Voltis <v.agolli@live.com>
2019-01-31 14:31:03 +0100

    details dynamic data

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
 import PropTypes from 'prop-types';
 
 class Product extends Component {
@@ -9,22 +10,28 @@ class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={() => console.log('clicked image container')}>
-                        <Link to="/details" >
-                            <img src={img} alt="product" className="card-img-top"></img>
-                        </Link>
-                        <button
-                            className="cart-btn"
-                            disabled={inCart ? true : false}
-                            onClick={() => { console.log('added to cart'); }}
-                        >
-                            {
-                                inCart
-                                    ? (<p className="text-capitalize mb-0" disabled>{" "} in cart</p>)
-                                    : (<i className="fas fa-cart-plus" />)
-                            }
-                        </button>
-                    </div>
+
+                    <ProductConsumer>
+                        {(value) => (
+                            <div className="img-container p-5" onClick={() => value.handleDetail(id)}>
+                                <Link to="/details" >
+                                    <img src={img} alt="product" className="card-img-top"></img>
+                                </Link>
+                                <button
+                                    className="cart-btn"
+                                    disabled={inCart ? true : false}
+                                    onClick={() => value.addToCart(id)}
+                                >
+                                    {
+                                        inCart
+                                            ? (<p className="text-capitalize mb-0" disabled>{" "} in cart</p>)
+                                            : (<i className="fas fa-cart-plus" />)
+                                    }
+                                </button>
+                            </div>
+                        )}
+                    </ProductConsumer>
+
                     {/* card footer */}
                     <div className="card-footer d-flex justify-content-between">
                         <p className="align-self-center mb-0">
@@ -35,6 +42,7 @@ class Product extends Component {
                             {price}
                         </h5>
                     </div>
+
                 </div>
             </ProductWrapper>
         );
--- src/context.js
+++ src/context.js
@@ -27,8 +27,16 @@ class ProductProvider extends Component {
         })
     }
 
-    handleDetail = () => {
-        console.log('hello from details');
+    getItem = (id) => {
+        const product = this.state.products.find(item => item.id === id);
+        return product;
+    }
+
+    handleDetail = (id) => {
+        const product = this.getItem(id);
+        this.setState(() => {
+            return { detailProduct: product }
+        })
     }
 
     addToCart = (id) => {
