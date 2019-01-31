e52193a01fecdfe48874acba21912971d1adc14b
Voltis <v.agolli@live.com>
2019-01-31 11:16:51 +0100

    product component update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
 
 class Product extends Component {
     render() {
+        const { id, title, img, price, inCart } = this.props.product;
         return (
-            <div>
-                <h3>hello from produt</h3>
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={() => console.log('clicked')}>
+                        <Link to="/details" >
+                            <img src={img} alt="product" className="card-img-top"></img>
+                        </Link>
+                    </div>
+                </div>
+            </ProductWrapper>
         );
     }
 }
+
+const ProductWrapper = styled.div`
+
+`
 
 export default Product;
