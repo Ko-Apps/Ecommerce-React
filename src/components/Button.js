8e137ae1d5bb68204e69939bd2b9e3bf1b720fd0
Voltis <v.agolli@live.com>
2019-01-31 14:11:02 +0100

    details page

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ export const ButtonContainer = styled.button`
 text-transform: capitalize;
 font-size: 1.4rem;
 background: transparent;
-border: 0.05rem solid var(--lighBlue);
+border: 0.05rem solid var(--lightBlue);
 color: var(--lightBlue);
 border-radius: 0.5rem;
 padding: 0.2rem 0.5rem;
@@ -12,7 +12,7 @@ cursor: pointer;
 margin: 0.2rem 0.5rem 0.2rem 0;
 transition: all 0.5s ease-in-out;
 &:hover{
-    background: var(--lighBlue);
+    background: var(--lightBlue);
     color:var(--mainBlue);
 }
 &:focus{
