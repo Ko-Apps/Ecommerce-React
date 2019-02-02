a4181db70e658e6e04212a77a68f03068c2c6b43
Voltis <v.agolli@live.com>
2019-02-02 23:09:57 +0100

    cart placeholders

--- src/context.js
+++ src/context.js
@@ -11,7 +11,10 @@ class ProductProvider extends Component {
         detailProduct,
         cart: [],
         modalOpen: false,
-        modalProduct: detailProduct
+        modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0
     }
 
     componentDidMount() {
@@ -66,6 +69,26 @@ class ProductProvider extends Component {
         this.setState({ modalOpen: false });
     }
 
+    increment = (id) => {
+        console.log('increment method');
+
+    }
+
+    decrement = (id) => {
+        console.log('decrement method');
+
+    }
+
+    removeItem = (id) => {
+        console.log('removed item');
+
+    }
+
+    clearCart = () => {
+        console.log('cart cleared');
+
+    }
+
     render() {
         return (
             <ProductContext.Provider
@@ -74,7 +97,11 @@ class ProductProvider extends Component {
                     handleDetail: this.handleDetail,
                     addToCart: this.addToCart,
                     openModal: this.openModal,
-                    closeModal: this.closeModal
+                    closeModal: this.closeModal,
+                    increment: this.increment,
+                    decrement: this.decrement,
+                    removeItem: this.removeItem,
+                    clearCart: this.clearCart
                 }}
             >
                 {this.props.children}
