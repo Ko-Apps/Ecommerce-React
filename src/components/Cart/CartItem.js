c2876dc2a8a0d496e5b1431a32e65b8b9ca84c4d
Voltis <v.agolli@live.com>
2019-02-03 00:10:07 +0100

    cart ccs

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-export default function CartItem() {
+export default function CartItem({ item, value }) {
+    const { id, title, img, price, total, count } = item;
+    const { increment, decrement, removeItem } = value;
     return (
-        <div>
-            hello from cart CartItem
-        </div>
+        <div className="row my-1 text-capitalize text-center">
+            <div className="col-10 mx-auto col-lg-2">
+                <img src={img} style={{ width: '5em', height: '5em' }} className="img-fluid" alt="product"></img>
+            </div>
+            <div className="col-10 mx-auto col-lg-2">
+                <span className="d-lg-none">product: </span>
+                {title}
+            </div>
+
+            <div className="col-10 mx-auto col-lg-2">
+                <span className="d-lg-none">price: </span>
+                {price}
+            </div>
+
+            <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+                <div className="d-flex justify-content">
+                    <div>
+                        <span className="btn btn-black mx-1" onClick={() => decrement(id)} >-</span>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
