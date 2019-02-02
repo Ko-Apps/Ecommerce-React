9c59282a37f88f87efff617fa43a834409b4f5a1
Voltis <v.agolli@live.com>
2019-02-02 23:32:58 +0100

    cart empty comp

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import Title from '../Title';
 import CartColums from './CartColumns';
-
+import EmptyCart from './EmptyCart';
+import { ProductConsumer } from '../../context';
 
 class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart" />
-                <CartColums />
+                <ProductConsumer>
+                    {value => {
+                        const { cart } = value;
+                        if (cart.length > 0) {
+                            return (
+                                <React.Fragment>
+                                    <Title name="your" title="cart" />
+                                    <CartColums />
+                                </React.Fragment>
+                            )
+                        }
+                        else {
+                            return (<EmptyCart />)
+                        }
+                    }}
+                </ProductConsumer>
+
             </section>
         );
     }
